7018540565ffb0f19a4d1e60f34686017fe56d05
abiola <abiola.arobieke@gmail.com>
2023-07-05 11:02:27 -0400

    Inherit the class person from nameable

--- classes/person.rb
+++ classes/person.rb
@@ -1,15 +1,16 @@
-# Define a base class
-class Person
+require './classes/nameable'
+
+class Person < Nameable
+   # accessor get methods
+   attr_reader :id
+   attr_accessor :name, :age
+   
   def initialize(age, name = 'Unknown', parent_permission: true)
     @id = Random.rand(1..1000)
     @name = name
     @age = age
     @parent_permission = parent_permission
   end
-
-  # accessor get methods
-  attr_reader :id
-  attr_accessor :name, :age
 
   # accessor set methods
   def of_age
