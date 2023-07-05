076e5c0f583f415084f264cd2f02910b13130e44
abiola <abiola.arobieke@gmail.com>
2023-07-05 10:02:13 -0400

    Update student.rb file

--- classes/student.rb
+++ classes/student.rb
@@ -2,8 +2,8 @@ require './classes/person'
 
 # Define a sub-base class
 class Student < Person
-  def initialize(classroom, age)
-    super(age)
+  def initialize(age, classroom, name = 'Unknown', parent_permission: true)
+    super(age, name, parent_permission: parent_permission)
     @classroom = classroom
   end
 
