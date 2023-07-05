b91b3dde8d201ec950271b52c030e50a0dacc660
abiola <abiola.arobieke@gmail.com>
2023-07-05 11:40:07 -0400

    Update the capitalize_decorator class

--- classes/capitalize_decorator.rb
+++ classes/capitalize_decorator.rb
@@ -1,7 +1,8 @@
 require './classes/base_decorator'
 
+# Class for capitalizing name
 class CapitalizeDecorator < BaseDecorator
-    def correct_name
-        @nameable.correct_name.capitalize
-    end
+  def correct_name
+    @nameable.correct_name.capitalize
+  end
 end
