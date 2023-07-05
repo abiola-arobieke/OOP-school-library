ff9bcb963e361aff513ebcff6ced5887e99af6f3
abiola <abiola.arobieke@gmail.com>
2023-07-05 11:05:08 -0400

    Add trimmer_decorator.rb file to the project

---
+++ classes/trimmer_decorator.rb
@@ -0,0 +1,13 @@
+require './classes/base_decorator'
+
+class TrimmerDecorator < BaseDecorator
+    MAX_LENGTH = 10
+    
+    def correct_name
+        if @nameable.correct_name.length <= MAX_LENGTH
+            @nameable.correct_name
+        else
+            @nameable.correct_name[0...MAX_LENGTH]
+        end
+    end
+end
