e8ea27c88bb0fd1d6afa15531b2ca215e67b1f1e
abiola <abiola.arobieke@gmail.com>
2023-07-07 13:55:10 -0400

    Add student.rb file to the project

--- classes/student.rb
+++ classes/student.rb
@@ -1,10 +1,11 @@
-require './classes/person'
+# # require './classes/person'
+require_relative 'person'
 
 # Define a sub-base class
 class Student < Person
   attr_reader :classroom
 
-  def initialize(age, classroom, name = 'Unknown', parent_permission: true)
+  def initialize(age:, classroom:, name: 'Unknown', parent_permission: true)
     super(age, name, parent_permission: parent_permission)
     @classroom = classroom
   end
@@ -18,3 +19,24 @@ class Student < Person
     classroom.students.push(self) unless classroom.students.include?(self)
   end
 end
+
+
+# require_relative 'person'
+
+# class Student < Person
+#   attr_reader :classroom
+
+#   def initialize(age:, classroom:, name: 'Unknown', parent_permission: true)
+#     super(name: name, age: age, parent_permission: parent_permission)
+#     @classroom = classroom
+#   end
+
+#   def classroom=(classroom)
+#     @classroom = classroom
+#     classroom.students.push(self)
+#   end
+
+#   def play_hooky
+#     '¯\(ツ)/¯'
+#   end
+# end
