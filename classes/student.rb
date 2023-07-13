960b95f6abbcc1f44720b810e66ec515697fc71c
abiola <abiola.arobieke@gmail.com>
2023-07-13 11:12:14 -0400

    Update student.rb file

--- classes/student.rb
+++ classes/student.rb
@@ -4,9 +4,9 @@ require_relative 'person'
 class Student < Person
   attr_reader :classroom
 
-  def initialize(age:, classroom:, name: 'Unknown', parent_permission: true)
+  def initialize(age, name = 'Unknown', parent_permission: true)
     super(age, name, parent_permission: parent_permission)
-    @classroom = classroom
+    @classroom = nil
   end
 
   def play_hooky
@@ -16,5 +16,15 @@ class Student < Person
   def classroom=(classroom)
     @classroom = classroom
     classroom.students.push(self) unless classroom.students.include?(self)
+  end
+
+  def to_hash
+    {
+      type: 'student',
+      id: @id,
+      age: @age,
+      name: @name,
+      parent_permission: @parent_permission
+    }
   end
 end
