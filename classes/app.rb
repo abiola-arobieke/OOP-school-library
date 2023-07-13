282a0624d086f2a68a0ecc1a0d3984e8275ce47c
Osman Alhassan <oalhassan847@gmail.com>
2023-07-13 16:16:29 -0700

    Remove comment

--- classes/app.rb
+++ classes/app.rb
@@ -30,7 +30,7 @@ module FileManager
     end
   end
 
-  def load_rentals # rubocop:disable Metrics/ClassLength
+  def load_rentals
     return unless File.exist?('../json/rentals.json') && !File.empty?('../json/rentals.json')
 
     rentals_data = JSON.parse(File.read('../json/rentals.json'))
