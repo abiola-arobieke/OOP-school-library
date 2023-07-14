02b6a03efb0facc67b7064b7f47ab822fff64a08
abiola <abiola.arobieke@gmail.com>
2023-07-14 18:40:57 -0400

    Add trimmer_decorator_spec.rb file to the project

---
+++ spec/trimmer_decorator_spec.rb
@@ -0,0 +1,22 @@
+require_relative '../classes/trimmer_decorator'
+
+RSpec.describe TrimmerDecorator do
+  let(:nameable) { double('Nameable') }
+  subject { described_class.new(nameable) }
+
+  describe 'check of the correct_name is working correctly' do
+    context 'test when the name is longer than 10 characters' do
+      it 'should trims the name down to the first 10 characters' do
+        allow(nameable).to receive(:correct_name) {'Maximilliano'}
+        expect(subject.correct_name).to eq('Maximillia')
+      end
+    end
+
+    context 'test when the name is not longer than 10 character' do
+      it 'should return the same name' do
+        allow(nameable).to receive(:correct_name) {'Sola Ali'}
+        expect(subject.correct_name).to eq('Sola Ali')
+      end
+    end
+  end
+end
