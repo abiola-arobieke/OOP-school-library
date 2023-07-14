ed0e135d1f80a02de571b42cfb8d71420e49ce01
abiola <abiola.arobieke@gmail.com>
2023-07-14 18:37:23 -0400

    Add capitalize_decorator_spec.rb file to the  project

---
+++ spec/capitalize_decorator_spec.rb
@@ -0,0 +1,13 @@
+require_relative '../classes/capitalize_decorator'
+
+RSpec.describe CapitalizeDecorator do
+  let(:nameable) { double('Nameable') }
+  subject { described_class.new(nameable) }
+
+  describe 'test the correct_name method to return a capitalize name' do
+    it 'should return a capitalized name' do
+      allow(nameable).to receive(:correct_name) {'seun joy'}
+      expect(subject.correct_name).to eq('Seun joy')
+    end
+  end
+end
